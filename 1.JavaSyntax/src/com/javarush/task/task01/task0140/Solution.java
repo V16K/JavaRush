23dacbba6650c580865e0966779eb7280f659657
VKoro <korolevsky.v@gmail.com>
2018-12-18 00:23:52 +0300

    Lvl UP =)

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0140;
+
+/* 
+Выводим квадрат числа
+*/
+
+public class Solution {
+    public static int number = 38;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.print(number * number);
+    }
+}
