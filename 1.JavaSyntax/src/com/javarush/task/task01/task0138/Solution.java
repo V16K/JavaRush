fcb39aa40d58347f5c04d9aff281142f83977ef2
VKoro <korolevsky.v@gmail.com>
2018-12-18 23:31:40 +0300

    1-8

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Я вернулся поздно — ");
+        System.out.println("Это признаю.");
+        System.out.println("Но это ещё не повод");
+        System.out.println("Брать в руки сковороду.");
+    }
+}
