97104d94c1017d4dd375618de4917219d4b6d89f
VKoro <korolevsky.v@gmail.com>
2018-12-20 00:14:34 +0300

    lvl 2

--- 1.JavaSyntax/src/com/javarush/task/task01/task0128/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0128/Solution.java
@@ -6,7 +6,9 @@ package com.javarush.task.task01.task0128;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a = 2;
+        int b = 2;
+        sum(a, b);
     }
 
     public static void sum(int a, int b) {
