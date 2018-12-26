156cc1338d266c73b39d3525755a37be757e0506
VKoro <korolevsky.v@gmail.com>
2018-12-26 23:24:29 +0300

    блин, нельзя на долго отлынивать! Витя - фу таким быть.

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task02.task0202;
+
+/* 
+Откуда берутся Person?
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Person person = new Person();
+    }
+
+    public static class Person {
+        //напишите тут ваш код
+        String name = "Vova";
+        int age = 25;
+        int weight = 75;
+        int money = 7500;
+    }
+}
