4da8d3c237f665624fab4c10e59709ec4f4292a2
Job <korolevsky.v@gmail.com>
2019-02-15 11:58:23 +0300

    4.10

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0433;
+
+
+/* 
+Гадание на долларовый счет
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        int i = 0;
+        String s = "SSSSSSSSSS";
+        while (i < 10) {
+            System.out.println(s);
+            i++;
+        }
+    }
+}
