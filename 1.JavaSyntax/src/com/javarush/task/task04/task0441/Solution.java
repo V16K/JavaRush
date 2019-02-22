a9aee35f79f17448a1305fad1af1248cdceafc5b
Job <korolevsky.v@gmail.com>
2019-02-22 11:08:41 +0300

    4.16

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0441;
+
+
+/* 
+Как-то средненько
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            a[i] = scanner.nextInt();
+        }
+        Arrays.sort(a);
+
+        System.out.println(a[1]);
+    }
+}
