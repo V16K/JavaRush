c71af6024ccc8a626d48272c86544181c942a546
VKoro <korolevsky.v@gmail.com>
2019-02-24 14:57:17 +0300

    lvl 5.5

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.name = "Murzik";
+        cat1.age = 3;
+        cat1.weight = 5;
+        cat1.strength = 1;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat0 = new Cat("Persik", 3, 5, 15);
+        Cat cat1 = new Cat("Murzik", 2, 3, 12);
+        Cat cat2 = new Cat("Murka", 4, 3, 10);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
